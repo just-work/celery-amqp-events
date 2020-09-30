55171bec75862477cb12230aafbfdbd094552320
tumb1er <zimbler@gmail.com>
2020-09-30 15:30:38 +0300

    Add tests for different ways of task registration

--- demo/celery.py
+++ demo/celery.py
@@ -1,7 +1,6 @@
-import celery
 from amqp_events.celery import events_app
 
-app: celery.Celery = events_app(
+app = events_app(
     'demo_consumer',
     broker_url='amqp://guest:guest@rabbitmq:5672/',
     imports=['demo.tasks'])
